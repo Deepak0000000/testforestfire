7229be360d156ccc3155839f0ceb647429fe0949
Deepak singh <deepakthakur0466@gmail.com>
2023-04-23 05:54:08 +0530

    first commit

--- application.py
+++ application.py
@@ -16,7 +16,8 @@ standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
 def index():
     return render_template('index.html')
 
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('
+',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='POST':
         Temperature=float(request.form.get('Temperature'))
